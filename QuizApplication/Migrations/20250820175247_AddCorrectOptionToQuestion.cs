1209718dc6a077c97d54e8f290879aed28f7d7cc
SandeepaniGunasekara <115693602+SandeepaniGunasekara@users.noreply.github.com>
2025-08-21 00:27:37 +0530

    commit1

---
+++ QuizApplication/Migrations/20250820175247_AddCorrectOptionToQuestion.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuizApplication.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCorrectOptionToQuestion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CoorectOption",
+                table: "Questions",
+                newName: "CorrectOption");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CorrectOption",
+                table: "Questions",
+                newName: "CoorectOption");
+        }
+    }
+}
--- QuizApplication/Program.cs
+++ QuizApplication/Program.cs
@@ -1,9 +1,85 @@
+using Microsoft.EntityFrameworkCore;
+using QuizApplication.Data;
+using QuizApplication.Models.Entities;
+using static System.Net.Mime.MediaTypeNames;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>options.UseSqlServer(connectionString));
 
 var app = builder.Build();
+using var scope = app.Services.CreateScope();
+var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+if (!dbContext.Questions.Any())
+{
+    var question1Answer = Guid.NewGuid();
+    var question1 = new Question()
+    {
+        Text = "What is the capital of New zealand",
+        Option = new List<Option>
+        {
+            new Option()
+            {
+                Id = Guid.NewGuid(),
+                Text = "Auckland"
+            },
+            new()
+            {
+                 Id = question1Answer,
+                Text = "Wellington"
+            },
+            new()
+            {
+                 Id = Guid.NewGuid(),
+                Text = "Christchurch"
+            },
+             new()
+            {
+                 Id = Guid.NewGuid(),
+                Text = "Queenstown"
+            }
+        },
+        CoorectOption = question1Answer
+    };
+
+    var question2Answer = Guid.NewGuid();
+    var question2 = new Question()
+    {
+        Text = "What is the capital of India",
+        Option = new List<Option>
+        {
+            new Option()
+            {
+                Id = Guid.NewGuid(),
+                Text = "Au"
+            },
+            new()
+            {
+                 Id = question2Answer,
+                Text = "New Delhi"
+            },
+            new()
+            {
+                 Id = Guid.NewGuid(),
+                Text = "Christchurch"
+            },
+             new()
+            {
+                 Id = Guid.NewGuid(),
+                Text = "Queenstown"
+            }
+        },
+        CoorectOption = question2Answer
+    };
+
+    //save the questions in the db
+    dbContext.Questions.AddRange([question1, question2]);
+    dbContext.SaveChanges();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
