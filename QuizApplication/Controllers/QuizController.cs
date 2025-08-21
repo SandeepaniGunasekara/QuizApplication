136af4857b7cf04a6ca487a96fbbf492ed6c2d88
SandeepaniGunasekara <115693602+SandeepaniGunasekara@users.noreply.github.com>
2025-08-21 13:00:29 +0530

    Defined viewmodels

---
+++ QuizApplication/Controllers/QuizController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuizApplication.Data;
+
+namespace QuizApplication.Controllers
+{
+    public class QuizController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+
+        public QuizController(ApplicationDbContext dbContext1) 
+        { 
+            this.dbContext = dbContext1;
+
+        }
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            dbContext.Questions.Include(x => x.Option).ToList(); //get the list of all the questions
+            return View();
+        }
+    }
+}
---
+++ QuizApplication/Models/ViewModels/QuizViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace QuizApplication.Models.ViewModels
+{
+    public class QuizViewModel
+    {
+        public required List<QuestionItems> Questions { get; set; } = [];
+    }
+
+    public class QuestionItems
+    {
+        public required Guid Id { get; set; }
+        public required string Text { get; set; }
+        public required List<SelectListItem> Options { get; set; }
+    }
+}
